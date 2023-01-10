031cfae52a5a61bac41e2d1fff18e93c29677247
Nadji Thabet <38954923+ThabetNadji@users.noreply.github.com>
2023-01-10 11:46:20 +0100

    Update question-services.service.ts
    publicUrlChanged

--- src/app/services/question-services.service.ts
+++ src/app/services/question-services.service.ts
@@ -10,7 +10,7 @@ export class QuestionServicesService {
   module:string=">";
   trimester:string=">";
   questionList : Array<question>=[];
-  publicUrl:string ='https://heroku-quizapi.herokuapp.com/';
+  publicUrl:string ='https://springquizappapi-production.up.railway.app/';
   //publicUrl:string ='http://localhost:8080/'; //https://heroku-quizapi.herokuapp.com/
   private UrlPost = "";
   constructor(private httpClient: HttpClient) {}
